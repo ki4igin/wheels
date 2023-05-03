44b45a593570442e75adc09308702d7fa2b29a0f
ki4igin <56039050+ki4igin@users.noreply.github.com>
2023-05-03 15:23:05 +0300

    Add ports (#5)
    * add ports
    
    * add ports info to readme

--- wheels/code/Src/udp_server.h
+++ wheels/code/Src/udp_server.h
@@ -4,6 +4,7 @@
 #include "stdint.h"
 
 void udp_server_init(void);
-void udp_server_send(void *data, uint32_t size);
+void udp_server_send_rtd(void *data, uint32_t size);
+void udp_server_send_vibr(void *data, uint32_t size);
 
 #endif
